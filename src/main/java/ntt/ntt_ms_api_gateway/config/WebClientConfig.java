d328dcfd08f53e240326f4528e3d3cc4d2eb3242
Fabian216 <fabian.alan.olaya@gmail.com>
2025-09-04 16:37:52 -0500

    first commit

---
+++ src/main/java/ntt/ntt_ms_api_gateway/config/WebClientConfig.java
@@ -0,0 +1,15 @@
+package ntt.ntt_ms_api_gateway.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder builder(){
+        return WebClient.builder();
+    }
+}
---
+++ src/test/java/ntt/ntt_ms_api_gateway/NttMsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ntt.ntt_ms_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttMsApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
